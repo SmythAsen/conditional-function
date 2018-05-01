83f3f913862246ef6d2c698167ec7799a0097ca9
huangyuansheng <huangyuansheng@yijiupi.cn>
2018-05-01 12:59:49 +0800

    first commit

---
+++ src/main/java/com/asen/util/Function.java
@@ -0,0 +1,15 @@
+package com.asen.util;
+
+/**
+ * @Decription: the function of IfFunction's key
+ * @Author: Asen
+ * @Date: Create in 17:35 2018/4/29 0029
+ * @Email: SmythAsen@gmail.com
+ **/
+public interface Function {
+	
+	/**
+	 * need to do something
+	 */
+	void invoke();
+}
